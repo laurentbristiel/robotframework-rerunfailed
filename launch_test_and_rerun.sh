81efc08649203f9dd449afff72e868667cf62eac
Laurent Bristiel <laurent@bristiel.com>
2015-02-25 09:39:16 +0100

    enhance script

--- launch_test_and_rerun.sh
+++ launch_test_and_rerun.sh
@@ -1,3 +1,4 @@
+# clean previous output files
 rm -f output/output.xml
 rm -f output/rerun.xml
 rm -f output/first_run_log.html
@@ -5,21 +6,18 @@ rm -f output/second_run_log.html
 
 pybot --outputdir output $@
 
-exit 0
-
 # we stop the script here if all the tests were OK
 if [ $? -eq 0 ]; then
 	echo "we don't run the tests again as everything was OK on first try"
 	exit 0	
 fi
-
 # otherwise we go for another round with the failing tests
 
 # we keep a copy of the first log file
 cp output/log.html  output/first_run_log.html
 
 # we launch the tests that failed
-pybot --nostatusrc --rerunfailed output/output.xml --output rerun.xml testcases
+pybot --outputdir output --nostatusrc --rerunfailed output/output.xml --output rerun.xml $@
 # => Robot Framework generates file rerun.xml
 
 # we keep a copy of the second log file
